214fdf0a957c143b81c7c5c538d5efd46af3c7de
Kiady <andriampianinana@gmail.com>
2024-03-08 13:52:45 +0300

    that an awesome build

--- README.md
+++
@@ -1,1 +0,0 @@
-# test_react_pwa
---
+++ build/precache-manifest.bf200bb1de327751fe52e73427e6f676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3958dabfcf358dce78cab11614205157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b0122d6c1e5520a86a4",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "3b0122d6c1e5520a86a4",
+    "url": "/static/js/main.b0271aef.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
